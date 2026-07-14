2221327bf94728c7b2c8c58396cd0f415743b771
delsonlife <44551767+delsonlife@users.noreply.github.com>
2026-07-14 01:44:25 +0100

    Create PageTransition.tsx

---
+++ components/ui/PageTransition.tsx
@@ -0,0 +1,17 @@
+// components/ui/PageTransition.tsx
+'use client'
+
+import { motion } from 'framer-motion'
+import { ReactNode } from 'react'
+
+export default function PageTransition({ children }: { children: ReactNode }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1], delay: 0.2 }}
+    >
+      {children}
+    </motion.div>
+  )
+}
